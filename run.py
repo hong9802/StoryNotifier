5e58c47b916d820d55af89a837904a1ce6782750
Hiroshi Lin <hlee993@gmail.com>
2020-07-20 23:58:30 +0900

    getpass 추가. 추후 다 갈아엎을 예정

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from src import login
 from src.JsonConfigManager import JsonConfigManager
 import time
+import getpass
 user_id = input("Kakao Story ID를 입력해주세요 : ")
-user_pw = input("PW를 입력해주세요 : ")
+user_pw = getpass.getpass()
 cookie = login.start(user_id, user_pw)
 while True:
     JsonConfigManager().checker()
